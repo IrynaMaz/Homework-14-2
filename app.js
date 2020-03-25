01a89d54a579c178300ade8d5c1260401e5a3396
Iryna <irynamazuryk@Irynas-MacBook-Air.local>
2020-03-25 17:47:17 +0200

    added task 3 and 4

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let numbers = [1,2,3,4,5];
 
+// 2
+
 let last_number = numbers.pop();
 console.log(last_number);
 
@@ -14,3 +16,13 @@ function sumInput() {
 	return sum;
 }
 console.log(sumInput() );
+
+// 3
+
+let numberMax=[4,5,3,2,6];
+console.log(Math.max(...numberMax));
+
+// 4
+
+let numberMin=[4,5,3,2,6];
+console.log(Math.min(...numberMin));
